7f7541d60924a6e14a0a54a7e84774a5e84c170d
Garrett <garrettshevach@gmail.com>
2022-12-11 19:30:59 -0500

    Updated env example and handles env setup in install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,13 @@
 GREEN="\033[0;32m"
 DOCKER_COMMAND="docker-compose exec --user=laradock workspace"
 
+cp .env.example .env
 cp .env.laradock laradock/.env
 cp nginx.conf laradock/nginx/sites/default.conf
 
 cd laradock
 docker-compose up -d --build nginx mysql redis
+$DOCKER_COMMAND php /var/www/artisan key:generate
 $DOCKER_COMMAND php /var/www/artisan migrate
 $DOCKER_COMMAND npm install
 $DOCKER_COMMAND npm run build
